965a74f96ec6bd40d694a47b48fcfe42cf3e273f
Magino <onigam@protonmail.com>
2026-04-20 19:46:47 +0200

    fix(web): install @sinclair/typebox in web package to satisfy format registry types

--- packages/web/src/components/constituents/constituent-form.tsx
+++ packages/web/src/components/constituents/constituent-form.tsx
@@ -1,13 +1,13 @@
-import { FormatRegistry } from "@sinclair/typebox/custom";
+"use client";
+
+import { FormatRegistry } from "@sinclair/typebox";
 
 // We must implement the email regex format manually for TypeBox in the browser if we don"t import the full formats plugin.
 if (!FormatRegistry.Has("email")) {
-  FormatRegistry.Set("email", (value) =>
+  FormatRegistry.Set("email", (value: string) =>
     /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value),
   );
 }
-
-("use client");
 
 import { ConstituentCreateSchema, ConstituentUpdateSchema } from "@givernance/shared/validators";
 import { typeboxResolver } from "@hookform/resolvers/typebox";
